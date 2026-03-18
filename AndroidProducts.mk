10f74de1bc21d068638a7390a4ef155c395d3731
kylieeXD <khayloaftck@gmail.com>
2026-03-18 02:42:03 +0000

    vendor: Add POCO X3 NFC (surya)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,4 +6,5 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/X1/X1.mk \
     $(LOCAL_DIR)/onyx/onyx.mk \
-    $(LOCAL_DIR)/topaz/topaz.mk
+    $(LOCAL_DIR)/topaz/topaz.mk \
+    $(LOCAL_DIR)/surya/surya.mk
---
+++ surya/surya.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2020-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
+
+# Inherit some common Lumine stuff.
+$(call inherit-product, vendor/lumine/config/common_full_phone.mk)
+
+# Lumine build flags
+TARGET_SCREEN_WIDTH := 1080
+TARGET_ENABLE_BLUR := true
+LUMINE_MAINTAINER := kylieeXD
+
+# Device manufacturer
+PRODUCT_NAME := surya
+PRODUCT_DEVICE := surya
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := M2007J20CG
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="surya_global-user 12 RKQ1.211019.001 V14.0.2.0.SJGMIXM release-keys" \
+    BuildFingerprint=POCO/surya_global/surya:12/RKQ1.211019.001/V14.0.2.0.SJGMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
